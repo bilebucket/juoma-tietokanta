a74e231efce6af9b516c5402f559a34bc30fedce
sapake <sapakapi@gmail.com>
2015-11-12 16:12:54 +0200

    valmiina esittelyä varten

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # coding=utf-8
 
 """
+
 Juoma-tietokanta
-TTZC0200.5S0V4 - Osaamisen näyttö
+TTZC0200.5S0V4 - Ohjelmoinnin Perusteet -  Osaamisen näyttö
+Sami Pitkänen 2015
+
 """
 
 # sisällytetään tarvittavat kirjastot
@@ -33,6 +36,7 @@ commands = [
 help_string = u"""
 
 Tervetuloa Juoma-tietokantaan!
+
 Tunnetut komennot:
     lopeta:    lopettaa ohjelman.
     kaikki:    tulostaa kaikkien juomien nimet.
@@ -45,7 +49,7 @@ Tunnetut komennot:
 
 """
 
-# Drink luokan data kentän malli
+# Drink luokan data taulukon malli
 data_template = OrderedDict(name=None, brewery=None, abv=None, points=None)
 
 
@@ -69,7 +73,7 @@ class Drink:
         return self.data['abv']
 
     def print_out(self):
-        # tulostetaan juoman tiedot kauniisti
+        # tulostetaan juoman tiedot siististi 
         print '\n{}.'.format(all_drinks.index(self))
         print '-' * len(self.get_name()) + '--'
         print '-' + self.get_name() + '-'
@@ -94,13 +98,13 @@ def print_all():
         print '{0}. {1}'.format(i, drink.get_name())
         i += 1
 
-suomennukset = {'name': 'nimi', 'brewery': 'panimo/valmistaja', 'abv': 'alkoholi tilavuusprosentti', 'points': 'pisteet' }
+suomennukset = {'name': 'nimi', 'brewery': 'panimo/valmistaja', 'abv': 'alkoholi tilavuusprosentti', 'points': 'pisteet (0.1-10.0)' }
 
 
 def add_drink():
     print '\n--UUSI JUOMA--\n'
     data = OrderedDict(data_template)
-    # kysytään käyttäjältä juoman tiedot ja lisätään syntyt Drink luokan instanssi all_drinks -listaan
+    # kysytään käyttäjältä juoman tiedot ja lisätään syntynyt Drink -luokan instanssi all_drinks -taulukkoon
     for field in data:
         data.update({field: raw_input(suomennukset[field] + '> ')})
     all_drinks.append(Drink(data))
@@ -127,8 +131,9 @@ def find():
 def db_init():
     global all_drinks
     print u'\nVAROITUS! TÄMÄ POISTAA KAIKKI JUOMAT TIETOKANNASTA.\nKOMENTOA EI VOI PERUTTAA.'
-    c = raw_input('JATKA [k/e]')
+    c = raw_input('JATKETAANKO [k/e]')
     if c.lower() == 'k':
+        print 'ALUSTETAAN...'
         all_drinks = [Drink({'name': None, 'brewery': None, 'abv': None, 'points': None})]
 
 
@@ -142,28 +147,39 @@ def delete():
 
 # avataan tietokanta ja luetaan data all_drinks listaan
 def init():
+    print '\nLuetaan tietokannasta...',
     f = open(DATABASE, 'ab')
     f.close()
     global all_drinks
     with open(DATABASE, 'rb') as handle:
         data = pickle.load(handle)
         all_drinks = data
+    print ' valmis!'
 
 
 # tallennetaan data, kun ohjelma suljetaan
 def save_data():
+    print '\nTallennetaan tiedot...',
     with open(DATABASE, 'wb') as handle:
         pickle.dump(all_drinks, handle)
+    print ' valmis!'
 
 
-# ohjelman "I/O" -looppi, jossa luetaan käyttäjän syöte ja suoritetaan sitten oikea funktiot
+# ohjelman "I/O" -looppi, jossa luetaan käyttäjän syöte ja suoritetaan sitten halutut funktiot
 def run():
     global running
+    
+    # tulostetaan helppi teksti
     print help_string
+
     while running:
         cmd = raw_input('\nkomento> ').lower()
+
+        # jos syöte ei ole tunnetuissa komennoissa, kerrotaan siitä käyttäjälle.
         if cmd not in commands:
             print 'Tuntematon komento "{}"'.format(cmd)
+        
+        # syöte on validi komento
         else:
             if cmd == 'lopeta':
                 save_data()
@@ -187,4 +203,5 @@ def run():
 if __name__ == '__main__':
     init()
     run()
-    pass
+    print '\nTervetuloa uudelleen!'
+
